25aa85035928249bc39a2761bcceb7840edec361
Joel Vallejo <joelcvallejo02@gmail.com>
2020-10-19 13:59:56 -0500

    Initial Commit

---
+++ whackAmole/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whackAmole
+//
+//  Created by Vallejo, Joel C on 10/19/20.
+//  Copyright © 2020 Vallejo, Joel C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
