cdc96940fa9e5bf4d087c76dfa2c0caef32cd380
Joel Vallejo <joelcvallejo02@gmail.com>
2020-10-21 13:46:54 -0500

    added background and score labels

--- whackAmole/ViewController.swift
+++ whackAmole/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var screenWidth = 0
+    var screenHeight = 0
+    var lblScore = UILabel()
+    var lblBackground = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let screenBounds: CGRect = UIScreen.main.bounds
+        screenWidth = Int(screenBounds.width)
+        screenHeight = Int(screenBounds.height)
+        
+        lblScore.frame = CGRect(x: 0, y: 0, width: (screenHeight/10), height: (screenHeight/10))
+        lblScore.text = "0"
+        
+        view.addSubview(lblScore)
+        
+        lblBackground.frame = CGRect(x: 0, y: (screenHeight/10), width: screenWidth, height: screenHeight-(screenHeight/10))
+        lblBackground.backgroundColor = UIColor.green
+        
+        view.addSubview(lblBackground)
     }
 
 
