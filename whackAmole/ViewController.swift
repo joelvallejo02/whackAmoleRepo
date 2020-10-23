d37e6fa857ab27dc7b30f783c65cfa739b182fcb
Joel Vallejo <joelcvallejo02@gmail.com>
2020-10-23 13:41:31 -0500

    movement

--- whackAmole/ViewController.swift
+++ whackAmole/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
     var screenHeight = 0
     var lblScore = UILabel()
     var lblBackground = UILabel()
+    var btnMole = UIButton()
+    var randomX = 0
+    var randomY = 0
+    var score = 0
+    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +36,39 @@ class ViewController: UIViewController {
         lblBackground.backgroundColor = UIColor.green
         
         view.addSubview(lblBackground)
+        
+        btnMole.frame = CGRect(x: (screenWidth/2), y: (screenHeight/10), width: 30, height: 30)
+        btnMole.backgroundColor = UIColor.brown
+        btnMole.addTarget(self, action: #selector(hitMe(_:)), for: .touchUpInside)
+        timer = Timer.scheduledTimer(timeInterval: 4.0, target: self,
+        selector: #selector(newMole), userInfo: nil, repeats: false)
+        
+        view.addSubview(btnMole)
+        self.view = view
+    }
+        @objc func hitMe(_ sender:UIButton!) {
+           print("Ouch!")
+           score += 10
+           btnMole.removeFromSuperview()
+            lblScore.text = ("\(score)")
+            
+            // new mole
+            randomX = Int.random(in: 20...(screenWidth - 20) - 30)
+            randomY = Int.random(in: 20...(screenHeight - 100))
+            btnMole.frame = CGRect(x: randomX, y: randomY, width: 30, height: 30)
+            view.addSubview(btnMole)
+           
+        }
+    @objc func newMole(_ sender:UIButton!){
+        timer = Timer.scheduledTimer(timeInterval: 4.0, target: self,
+        selector: #selector(newMole), userInfo: nil, repeats: false)
+        // new mole
+        randomX = Int.random(in: 20...(screenWidth - 20) - 30)
+        randomY = Int.random(in: 100...(screenHeight - 100))
+        btnMole.frame = CGRect(x: randomX, y: randomY, width: 30, height: 30)
+        view.addSubview(btnMole)
+        
+       
     }
 
 
